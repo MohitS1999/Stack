b0380da606158c87aab2196c90c89545d346ab59
Mohit singh <singhmohit9718@gmail.com>
2020-07-13 21:12:51 +0530

    Basic

---
+++ NearestGreaterToLeft.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+public class NearestGreaterToLeft{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int t=Integer.parseInt(br.readLine().trim());
+		while (t-->0){
+			int n=Integer.parseInt(br.readLine().trim());
+			String[] str=br.readLine().split(" ");
+			int ar[]=new int[n];
+			for (int i=0;i<n;i++)
+				ar[i]=Integer.parseInt(str[i]);
+			List<Integer> list=new ArrayList<>();
+			Stack<Integer> st=new Stack<>();
+			for (int i=0;i<n;i++){
+				while (!st.isEmpty() && ar[i]>=st.peek()) st.pop();				
+				if (st.isEmpty()) list.add(-1);
+				else list.add(st.peek());
+				st.push(ar[i]);
+			}
+			for (int i=0;i<n;i++) System.out.print(list.get(i)+" ");
+			System.out.println();
+		}
+	}
+}
