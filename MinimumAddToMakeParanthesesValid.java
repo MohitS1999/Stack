dc35d877013a4ae6e783392655e7c5a0b4cfd4ab
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-07-23 22:36:01 +0530

    Basic

---
+++ MinimumAddToMakeParanthesesValid.java
@@ -0,0 +1,23 @@
+//https://www.hackerearth.com/practice/data-structures/stacks/basics-of-stacks/practice-problems/algorithm/minimum-add-to-make-parentheses-valid-9cba6259/
+import java.util.*;
+import java.util.Stack;
+class MinimumAddToMakeParanthesesValid{
+    public static void main(String args[] ) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int cnt=0;
+        Stack<Integer> st=new Stack<>();
+        // System.out.println(st.size());
+        for (char c:s.toCharArray()){
+            if (c=='('){
+                st.push(1);
+				//System.out.println(c);
+				}
+            else if ((!st.isEmpty()) && c==')' && st.peek()==1)
+                st.pop();
+            else if ((st.isEmpty()) && c==')')
+                cnt++;
+        }
+        System.out.println(cnt+st.size());
+    }
+}
